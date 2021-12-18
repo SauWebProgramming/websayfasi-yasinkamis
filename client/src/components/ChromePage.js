9517b8d25f705334ee242dae4483022d53ffb0f6
Yasin Kamış <74969496+yasinkamis@users.noreply.github.com>
2021-12-18 23:13:18 +0300

    google page add

--- client/src/components/ChromePage.js
+++ client/src/components/ChromePage.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Alt from "../assets/files/alt.png"
 import Close from "../assets/files/çık.png"
+import Facebook from './Facebook'
+import Google from './GooglePage'
 
 const ChromePage = () => {
+    const linkFacebook = "www.facebook.com"
+    const linkGoogle = "www.google.com"
+    const [linkChange, setLinkChange] = useState("www.google.com")
+    const [linkSearch, setLinkSearch] = useState("www.google.com")
     return (
         <div className='container-fluid chromePage'>
             <div className='chromePage_upper'>
@@ -13,7 +19,26 @@ const ChromePage = () => {
                     <img src={Close} alt="X" />
                 </div>
             </div>
-           
+            <div className='chromePage_url-box'>
+                <input type="text" placeholder={"Bir URL yazın"} defaultValue={linkSearch} onChange={(e) => { setLinkChange(e.target.value) }} />
+                <button type='button' onClick={ () => { setLinkSearch(linkChange) }}>Ara</button>
+            </div>
+            <div className='chromePage_page-box'>
+                {
+                    (
+                        (linkSearch === linkFacebook)
+                        &&
+                        <Facebook />
+                    ),
+                    (
+                        (linkSearch === linkGoogle)
+                        &&
+                        <Google />
+                    )
+
+
+                }
+            </div>
         </div>
     )
 }
---
+++ client/src/components/Facebook.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Facebook = () => {
+    return (
+        <div>
+            saasdasda
+            sda
+            sd
+            as
+            const [asd
+            a
+            sd
+            asd
+            as
+            d] = arrayToDestruct
+        </div>
+    )
+}
+
+export default Facebook
