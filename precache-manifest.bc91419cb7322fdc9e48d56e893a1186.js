b2b8a83a94787b37ca1ef306061b3db15282006a
Yasin Kamış <74969496+yasinkamis@users.noreply.github.com>
2021-12-27 15:28:43 +0300

    Updates

---
+++ precache-manifest.bc91419cb7322fdc9e48d56e893a1186.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2e8a85bbcb49587f77fa2b5a34e89e",
+    "url": "/websayfasi-yasinkamis/index.html"
+  },
+  {
+    "revision": "26f2d9a156731048cd90",
+    "url": "/websayfasi-yasinkamis/static/css/main.aa84cbde.chunk.css"
+  },
+  {
+    "revision": "ed4bc29c19915497e0db",
+    "url": "/websayfasi-yasinkamis/static/js/2.af5776a8.chunk.js"
+  },
+  {
+    "revision": "26f2d9a156731048cd90",
+    "url": "/websayfasi-yasinkamis/static/js/main.b60ed32c.chunk.js"
+  },
+  {
+    "revision": "46b09a0c8f843c14e208",
+    "url": "/websayfasi-yasinkamis/static/js/runtime~main.0693346d.js"
+  },
+  {
+    "revision": "9a36a65bae51a742955bb52c48a15c0a",
+    "url": "/websayfasi-yasinkamis/static/media/background.9a36a65b.png"
+  },
+  {
+    "revision": "2ebcb5e5166cd7dc128cabe319a40b89",
+    "url": "/websayfasi-yasinkamis/static/media/find.2ebcb5e5.png"
+  },
+  {
+    "revision": "897a6c7a93d098ab2626f2533cd4ff63",
+    "url": "/websayfasi-yasinkamis/static/media/hello.897a6c7a.png"
+  },
+  {
+    "revision": "fa430e1ce99c8df86fbd2e9f4637e9a9",
+    "url": "/websayfasi-yasinkamis/static/media/nav-resim.fa430e1c.png"
+  },
+  {
+    "revision": "84ac92e076fdac51983a6dc04053a11a",
+    "url": "/websayfasi-yasinkamis/static/media/post-1.84ac92e0.png"
+  },
+  {
+    "revision": "d939a691c4cb708cf33814bc3f6da908",
+    "url": "/websayfasi-yasinkamis/static/media/post-2.d939a691.png"
+  },
+  {
+    "revision": "bfe34935ab1501a162c9a3f59e365c48",
+    "url": "/websayfasi-yasinkamis/static/media/post-3.bfe34935.png"
+  },
+  {
+    "revision": "cc0e69e7f7714c596c9d35f1120ab3aa",
+    "url": "/websayfasi-yasinkamis/static/media/post-4.cc0e69e7.png"
+  },
+  {
+    "revision": "9b02c8a2f45751bd1537505b22bad228",
+    "url": "/websayfasi-yasinkamis/static/media/post-5.9b02c8a2.png"
+  },
+  {
+    "revision": "b1b02d78ed671fad639efb04e6a5fca0",
+    "url": "/websayfasi-yasinkamis/static/media/post-6.b1b02d78.png"
+  },
+  {
+    "revision": "0d71c33d0b2a517abb4706cf6ee3cbf3",
+    "url": "/websayfasi-yasinkamis/static/media/profile.0d71c33d.png"
+  },
+  {
+    "revision": "d94b0d163447a31049a73eda19406483",
+    "url": "/websayfasi-yasinkamis/static/media/volume.d94b0d16.png"
+  },
+  {
+    "revision": "eced415a6b0823162da98c62ad965e94",
+    "url": "/websayfasi-yasinkamis/static/media/wifi.eced415a.png"
+  },
+  {
+    "revision": "b47b575df12d1fad011b7d2395a581b5",
+    "url": "/websayfasi-yasinkamis/static/media/windows.b47b575d.png"
+  }
+]);
